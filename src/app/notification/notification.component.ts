c21b6ed7236f92bb47c1708257e555cce982f4f8
MythoAway7 <58316242+MythoAway7@users.noreply.github.com>
2022-06-28 13:31:53 -0500

    Add notifications

---
+++ src/app/notification/notification.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+@Component({
+    selector: 'app-notification',
+    templateUrl: './notification.component.html',
+    styleUrls: ['./notification.component.css']
+})
+export class NotificationComponent {
+    constructor(private _snackBar: MatSnackBar) {
+        this._snackBar.open("This is a custom notification!", "Close", {
+            panelClass: ["bg-blue-400"],
+            duration: 7000
+        });
+    }
+}
