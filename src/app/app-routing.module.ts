855a7a15ab28e24f6d50f5432915611b59cc9fb3
MythoAway7 <58316242+MythoAway7@users.noreply.github.com>
2022-07-27 19:24:01 -0500

    Remove import page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,10 +12,6 @@ const routes: Routes = [
             import('./remove-board/remove-board.module').then((m) => m.RemoveBoardModule)
     },
     {
-        path: "modifyboard/create", loadChildren: () =>
-            import('./import-board/import-board.module').then((m) => m.ImportBoardModule)
-    },
-    {
         path: '',
         loadChildren: () =>
             import('./board/board.module').then((m) => m.BoardModule),
