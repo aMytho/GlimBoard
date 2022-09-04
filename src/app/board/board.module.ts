6beb630d1d2f8490d4ee7b7bf493cffe2d24467e
MythoAway7 <58316242+MythoAway7@users.noreply.github.com>
2022-09-03 19:04:48 -0500

    Imports cleanup

--- src/app/board/board.module.ts
+++ src/app/board/board.module.ts
@@ -3,30 +3,20 @@ import { CommonModule } from '@angular/common';
 import { BoardComponent } from './board.component';
 import { BoardRoutingModule } from './board-routing.module';
 import { ButtonModule } from './button/button.module';
-import { AddButtonComponent } from './add-button/add-button.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { KtdGridModule } from "@katoid/angular-grid-layout";
-import { InstructionComponent } from './form/instruction/instruction.component'
-import { CastPipe } from './form/formPipe';
-import { DimensionsComponent } from './form/dimensions/dimensions.component';
-import { MessageComponent } from './form/instruction/message/message.component';
-import { MediaComponent } from './form/instruction/media/media.component';
-import { CommandComponent } from './form/instruction/command/command.component';
+import { CastPipe } from './button-form/pipes/form-pipe';
 import { BoardSettingsComponent } from './board-settings/board-settings.component';
+import { ButtonFormComponent } from './button-form/button-form.component';
 
 
 
 @NgModule({
   declarations: [
     BoardComponent,
-    AddButtonComponent,
-    InstructionComponent,
-    CastPipe,
-    DimensionsComponent,
-    MessageComponent,
-    MediaComponent,
-    CommandComponent,
-    BoardSettingsComponent
+    BoardSettingsComponent,
+    ButtonFormComponent,
+    CastPipe
   ],
   imports: [
     CommonModule,
