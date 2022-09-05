a496f2769c95d94f4c2c7d8dd4226ef283d4cef8
MythoAway7 <58316242+MythoAway7@users.noreply.github.com>
2022-09-05 16:05:11 -0500

    Add more instrutions and URL

--- src/app/info/info.component.ts
+++ src/app/info/info.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { ApiService } from '../core/api.service';
 
 @Component({
-  selector: 'app-info',
-  templateUrl: './info.component.html',
-  styleUrls: ['./info.component.css']
+    selector: 'app-info',
+    templateUrl: './info.component.html',
+    styleUrls: ['./info.component.css']
 })
-export class InfoComponent implements OnInit {
+export class InfoComponent {
+    public savedURL: string;
+    constructor(
+        private apiService: ApiService,
+        private notification: MatSnackBar
+    ) {
+        this.savedURL = this.apiService.getUrl();
+    }
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+    public saveURL(val: string) {
+        if (val) {
+            this.apiService.setURL(val);
+            this.notification.open("URL Saved!", "Close", {
+                panelClass: ["bg-green-400"],
+                duration: 5000
+            });
+        } else {
+            this.notification.open("You must enter a value!", "Close", {
+                panelClass: ["bg-red-400"],
+                duration: 5000
+            });
+        }
+    }
 }
