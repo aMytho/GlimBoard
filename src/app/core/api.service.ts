c2fa801b5661ffa9a1bf171760831a2d069c4d7d
MythoAway7 <58316242+MythoAway7@users.noreply.github.com>
2022-09-05 16:04:58 -0500

    Remove websocket

--- src/app/core/api.service.ts
+++ src/app/core/api.service.ts
@@ -1,6 +1,4 @@
-import { EventEmitter, Injectable } from '@angular/core';
-import { ConfigService } from './config.service';
-import { OutgoingPacket } from './types/packet';
+import { Injectable } from '@angular/core';
 
 @Injectable({
     providedIn: 'root'
@@ -8,64 +6,13 @@ import { OutgoingPacket } from './types/packet';
 export class ApiService {
     ws!: WebSocket;
     publicisConnected = false;
-    private emitter = new EventEmitter<any>();
-    constructor(
-        private configService: ConfigService
-    ) { }
+    private URL: string = localStorage.getItem("URL") || "localhost";
+    constructor() { }
 
-
-    connect() {
-        return new Promise(resolve => {
-            this.ws = new WebSocket(
-                this.configService.get("url", "http://localhost:4201/api/", false)
-                );
-            this.ws.onopen = (data) => {
-                resolve(true)
-                this.onConnect(data);
-            }
-            this.ws.onmessage = (data) => this.onMessage(JSON.parse(data.data));
-            this.ws.onclose = (data) => this.onClose(data.reason, data.code);
-            console.log("Listeners set, all good");
-            if (true) {
-                resolve(true)
-            }
-        })
-    }
-
-    onConnect(data: any) {
-        console.log("Connected to the api", data);
-    }
-
-    onMessage(message: any) {
-        console.log(message);
-        this.emitter.emit(message);
-    }
-
-    onClose(reason: string, code: number) {
-        console.log("Connection closed", reason, code)
-    }
-
-    send(data: OutgoingPacket) {
-        this.ws.send(JSON.stringify(data));
-    }
-
-    awaitResponse(message:OutgoingPacket, ID: number) {
-        return new Promise(resolve => {
-            // Send the message
-            this.ws.send(JSON.stringify(message));
-            // Once we get a response return the value.
-            this.emitter.subscribe((val) => {
-                console.log(val);
-                if (val.ID == ID) {
-                    resolve(val);
-                }
-            })
-        })
-    }
 
     async sendHTTPRequest(endpint: string, method: string, body: any = {}) {
         try {
-            let url = this.configService.get("url", "http://localhost:4201/api/", false);
+            let url = `http://${this.URL || "localhost"}:4201/api/`;
             let response = await fetch(url + endpint, {
                 method: method,
                 body: this.getCheck(method, body)
@@ -88,5 +35,14 @@ export class ApiService {
     getCheck(method: string, body: any) {
         if (method == "GET") return undefined;
         return JSON.stringify(body);
+    }
+
+    setURL(url: string) {
+        this.URL = url;
+        localStorage.setItem("URL", url);
+    }
+
+    getUrl(): string {
+        return this.URL || "localhost";
     }
 }
